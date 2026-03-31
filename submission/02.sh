5c22f229956cefb951221467f74ac9f0fc1d280d
Nwike Chisom <nwikechisom77@gmail.com>
2026-03-31 09:25:46 +0100

    add wallet creation and transaction handling scripts

--- submission/02.sh
+++ submission/02.sh
@@ -1,1 +1,4 @@
 # Create a native segwit address and get the public key from the address.
+segwit_addres=$(bitcoin-cli getnewaddress "" bech32)
+pubkey=$(bitcoin-cli getaddressinfo "$segwit_addres" | jq -r .pubkey)
+echo $pubkey
